69ea755e1e90983e77d61bff0633631eb005f34d
Nathan Lamont <nathan@alipes.com>
2021-05-24 23:20:40 -0400

    initial testing

---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container mx-auto">
+    <div>
+      <nuxt-content :document="page" />
+      <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div
+            class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+          >
+            <div
+              class="
+                shadow
+                overflow-hidden
+                border-b border-gray-200
+                sm:rounded-lg
+              "
+            >
+              <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Description
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr v-for="episode in episodes.body" :key="episode.ID">
+                    <td class="px-6 py-4">
+                      <a :href="episode.URL" target="_blank">
+                        {{ episode.Title }}
+                      </a>
+                    </td>
+                    <td class="px-6 py-4">
+                      {{ episode.Description }}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, store }) {
+    const page = await $content('hello').fetch()
+    const episodes = await $content('episodes').fetch()
+    return {
+      page,
+      episodes,
+    }
+  },
+}
+</script>
