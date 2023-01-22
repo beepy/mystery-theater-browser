d88e1ea0fa898931e91af3142df32964a836e776
Nathan Lamont <nathan@bigggerplanet.com>
2023-01-22 18:49:27 -0500

    test 5

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-      class="max-w-4xl mx-auto bg-white md:mt-10 p-8 md:rounded-lg shadow overflow-hidden"
-    >
+    class="max-w-4xl mx-auto bg-white md:mt-10 p-8 md:rounded-lg shadow overflow-hidden"
+  >
     <p>Hello World</p>
     <pre v-if="data">{{ data.data }}</pre>
     <p v-else><em>Not found</em></p>
@@ -9,9 +9,11 @@
 </template>
 
 <script lang="ts" setup>
-const data = ref(useAsyncData('home', async () => {
-  const d =  await queryContent('episodes').only(['id']).find()
-  return d.length
-}))
-console.log(data)
+const data = ref(
+  useAsyncData('home', async () => {
+    const d = await queryContent('episodes').only(['id']).find();
+    return d.length;
+  })
+);
+console.log(data);
 </script>
