f5f7460f75d8522f7018010668cef585e9ac0794
Nathan Lamont <nathan@bigggerplanet.com>
2023-01-22 18:47:00 -0500

    test 2

--- pages/episode/[slug].vue
+++ pages/episode/[slug].vue
@@ -1,7 +1,7 @@
 <template>
   <div
-      class="max-w-4xl mx-auto bg-white md:mt-10 p-8 md:rounded-lg shadow overflow-hidden"
-    >
+    class="max-w-4xl mx-auto bg-white md:mt-10 p-8 md:rounded-lg shadow overflow-hidden"
+  >
     <p>Episode {{ slug }}</p>
     <pre v-if="data?.data?.title">{{ data.data.title }}</pre>
     <p v-else><em>Not found</em></p>
@@ -9,17 +9,20 @@
   </div>
 </template>
 <script setup lang="ts">
-const route = useRoute()
-const slug = route.params.slug
-let nextSlug: number | undefined = typeof(slug) === 'string' ? parseInt(slug, 10) : undefined
+const route = useRoute();
+const slug = route.params.slug;
+let nextSlug: number | undefined =
+  typeof slug === "string" ? parseInt(slug, 10) : undefined;
 
-const data = ref(await useAsyncData(`episode-${route.params.slug}`, () => {
-  return queryContent(`episodes/${route.params.slug}`).findOne()
-}))
+const data = ref(
+  await useAsyncData(`episode-${route.params.slug}`, () => {
+    return queryContent(`episodes/${route.params.slug}`).findOne();
+  })
+);
 
 if (!isNaN(nextSlug)) {
-  nextSlug = nextSlug + 1
+  nextSlug = nextSlug + 1;
 } else {
-  nextSlug = undefined
+  nextSlug = undefined;
 }
 </script>
