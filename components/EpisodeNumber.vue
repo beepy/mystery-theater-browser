578c62175ee832eeb2d36e7a75b2f55ae9dc4c7f
Nathan Lamont <nathan@bigggerplanet.com>
2023-01-23 19:24:43 -0500

    episode number options => composition

--- components/EpisodeNumber.vue
+++ components/EpisodeNumber.vue
@@ -3,18 +3,26 @@
     {{ paddedNumber }}
   </span>
 </template>
-<script>
-export default defineComponent({
-  props: {
-    number: {
-      type: Number,
-      default: 0,
-    },
-  },
-  computed: {
-    paddedNumber() {
-      return String(this.number).padStart(4, '0');
-    },
-  },
+<script lang="ts" setup>
+// export default defineComponent({
+//   props: {
+//     number: {
+//       type: Number,
+//       default: 0,
+//     },
+//   },
+//   computed: {
+//     paddedNumber() {
+//       return String(this.number).padStart(4, '0');
+//     },
+//   },
+// });
+
+const props = defineProps<{
+  number: number;
+}>();
+
+const paddedNumber = computed(() => {
+  return String(props.number).padStart(4, '0') + 'x';
 });
 </script>
